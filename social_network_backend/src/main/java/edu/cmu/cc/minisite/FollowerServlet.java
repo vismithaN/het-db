e4b24179ff8b72ca2d5d8c08fe1f540468a676e1
Vismitha N <vismitha944@gmail.com>
2024-11-02 14:05:21 -0400

    Changed id

--- social_network_backend/src/main/java/edu/cmu/cc/minisite/FollowerServlet.java
+++ social_network_backend/src/main/java/edu/cmu/cc/minisite/FollowerServlet.java
@@ -9,12 +9,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
+
+import org.neo4j.driver.v1.*;
 import org.neo4j.driver.v1.Record;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.driver.v1.StatementResult;
 
 /**
  * Task 2:
@@ -106,21 +103,20 @@ public class FollowerServlet extends HttpServlet {
      */
     public JsonArray getFollowers(String id) {
         JsonArray followers = new JsonArray();
-        String query = "MATCH (follower:User)-[r:FOLLOWS]->(followee:User) \n" +
-                "WHERE followee.username = id\n" +
-                "RETURN follower.username, follower.url \n" +
-                "ORDER BY follower.username asc";
+        String query = "MATCH (follower:User)-[:FOLLOWS]->(followee:User) " +
+                "WHERE followee.id = $id " +
+                "RETURN follower.username AS username, follower.url AS url " +
+                "ORDER BY follower.username ASC";
         try (Session session = driver.session()) {
-            StatementResult rs = session.run(query);
+            StatementResult rs = session.run(query, Values.parameters("id", id));
             while (rs.hasNext()) {
                 Record record = rs.next();
                 JsonObject follower = new JsonObject();
-                follower.addProperty("name", record.get("username").toString());
-                follower.addProperty("profile", record.get("url").toString());
+                follower.addProperty("name", record.get("username").asString());
+                follower.addProperty("profile", record.get("url").asString());
                 followers.add(follower);
-                System.out.println(record.get(0).asString());
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return followers;
